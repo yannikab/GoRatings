1132e8f8f1d453a7111b1ee65022f167b040b44c
Yannis Kabilafkas <yannis.kabilafkas@gmail.com>
2023-02-23 02:12:18 +0200

    Rating contracts and controller.

---
+++ GoRatings.Api/Contracts/Rating/RatingResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace GoRatings.Contracts;
+
+public class RatingResponse
+{
+    public Guid EntityUid { get; set; }
+    public string EntityType { get; set; } = string.Empty;
+    public DateTime CalculatedDT { get; set; }
+    public int ConsideredRatings { get; set; }
+    public decimal OverallRating { get; set; }
+}
---
+++ GoRatings.Api/Controllers/RatingsController.cs
@@ -0,0 +1,22 @@
+﻿using GoRatings.Contracts;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoRatings.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RatingsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateRating(CreateRatingRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("{entityUid:guid}")]
+    public IActionResult GetRating(Guid entityUid)
+    {
+        return Ok(entityUid);
+    }
+}
